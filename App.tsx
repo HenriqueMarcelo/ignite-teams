ae55e47cfb65cf7c1db0962c99a922750c64164f
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-06-14 17:11:30 -0300

    style: create theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import { Groups } from '@screens/Groups'
+import { StatusBar } from 'expo-status-bar'
+import { ThemeProvider } from 'styled-components/native'
+import theme from './src/theme'
 
 export default function App() {
-  return <Groups />
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+      <StatusBar style="light" />
+    </ThemeProvider>
+  )
 }
