864365b9f3212d137d195af4372d21f7ee348322
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-06-14 17:18:24 -0300

    style: type for styled-components

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,13 +2,12 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  background-color: midnightblue;
-  align-items: center;
-  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `
 
 export const MainText = styled.Text`
-  color: lightcyan;
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
   font-size: 30px;
   font-weight: bold;
 `
