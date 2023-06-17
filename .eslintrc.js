ed5e00dff536f0144d9638d6c2ba33fa41d71f47
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-06-17 11:21:25 -0300

    feat: create ListEmpty component

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'no-unused-vars': 1,
     'prefer-template': 1,
     'react/jsx-key': 1,
+    'prettier/prettier': 1,
     camelcase: 0,
   },
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,13 @@ import { Hightlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
-  const [groups, setGroups] = useState(['Galera da Rocket', 'Galera da Pelada'])
+  const [groups, setGroups] = useState([
+    // 'Galera da Rocket',
+    // 'Galera da Pelada',
+  ])
 
   return (
     <Container>
@@ -17,6 +21,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={!groups.length && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   )
