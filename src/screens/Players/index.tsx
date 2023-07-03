a4157260909df58501b1b6e691fd2fc214e57432
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-03 13:56:24 -0300

    feat: create input and icon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from '@components/Header'
-import { Container } from './styles'
+import { Container, Form } from './styles'
 import { Hightlight } from '@components/Highlight'
 import { ButtonIcon } from '@components/ButtonIcon'
+import { Input } from '@components/Input'
 
 export function Players() {
   return (
@@ -12,8 +13,10 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe o time"
       />
-
-      <ButtonIcon type="PRIMARY" icon="add-chart" />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon type="PRIMARY" icon="add" />
+      </Form>
     </Container>
   )
 }
