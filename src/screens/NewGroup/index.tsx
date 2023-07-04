4e36017b20e37557907bf85955abe7f552df79e6
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-04 14:34:38 -0300

    feat: show errors

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,14 +6,30 @@ import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
 import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
+import { Alert } from 'react-native'
 
 export function NewGroup() {
   const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
   async function handleNew() {
-    await groupCreate(group)
-    navigation.navigate('players', { group })
+    const groupName = group.trim()
+    try {
+      if (groupName.length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome do grupo.')
+      }
+
+      await groupCreate(groupName)
+      navigation.navigate('players', { group: groupName })
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.log(error)
+      }
+    }
   }
 
   return (
