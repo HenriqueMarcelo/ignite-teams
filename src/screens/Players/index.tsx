e430b96469edbefc55c9a7ab0101b3ba30b03fde
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-04 12:14:13 -0300

    feat: pass parms into routes

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,6 +9,11 @@ import { useState } from 'react'
 import { PlayerCard } from '@components/PlayerCard'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { useRoute } from '@react-navigation/native'
+
+type RouteParams = {
+  group: string
+}
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
@@ -19,14 +24,14 @@ export function Players() {
     'Ana Lima',
   ])
 
+  const route = useRoute()
+  const { group } = route.params as RouteParams
+
   return (
     <Container>
       <Header showBackButton />
 
-      <Hightlight
-        title="Nome da turma"
-        subtitle="adicione a galera e separe o time"
-      />
+      <Hightlight title={group} subtitle="adicione a galera e separe o time" />
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon type="PRIMARY" icon="add" />
