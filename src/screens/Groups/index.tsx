851e87185761bfb7e10baaed5581a0581f83088c
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-04 11:44:26 -0300

    feat: create type for navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,12 +6,19 @@ import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 export function Groups() {
   const [groups, setGroups] = useState([
     // 'Galera da Rocket',
     // 'Galera da Pelada',
   ])
+
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -28,7 +35,7 @@ export function Groups() {
         )}
         showsVerticalScrollIndicator={false}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
