52ed46dcb99df527ae90784afaf97c31f4ada6cc
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-04 11:57:23 -0300

    feat: return to home

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
   extends: '@rocketseat/eslint-config/react',
   rules: {
-    'no-unused-vars': 1,
+    'no-unused-vars': 'off',
     'prefer-template': 1,
     'react/jsx-key': 1,
     'prettier/prettier': 1,
     camelcase: 0,
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
   },
 }
