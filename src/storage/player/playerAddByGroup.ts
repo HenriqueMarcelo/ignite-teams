436c06cf99339145831e05e79dfb2cfef124d9eb
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-05 11:08:16 -0300

    feat: add player function

---
+++ src/storage/player/playerAddByGroup.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { PLAYER_COLLECTION } from '@storage/storageConfig'
+import { AppError } from '@utils/AppError'
+import { PlayerStorageDTO } from './PlayerStorageDTO'
+import { playersGetByGroup } from './playersGetByGroup'
+
+export async function playerAddByGroup(
+  newPlayer: PlayerStorageDTO,
+  group: string,
+) {
+  const storedPlayers = await playersGetByGroup(group)
+
+  const playerAlreadyExists = storedPlayers.filter(
+    (player) => player.name === newPlayer.name,
+  )
+
+  if (playerAlreadyExists) {
+    throw new AppError('Essa pessoa já está adicionada em um time aqui.')
+  }
+
+  const store = JSON.stringify([...storedPlayers, newPlayer])
+  await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, store)
+}
